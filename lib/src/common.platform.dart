72e5b60bfacd666e350b1ff7d2e7dcabf2899e65
gordianknot <gordianknot.data@gmail.com>
2023-02-08 17:27:39 +0800

    Fix conditional import

--- lib/src/common.platform.dart
+++ lib/src/common.platform.dart
@@ -2,4 +2,4 @@ library platform;
 
 export 'sketch/platform.loader.dart'
 if (dart.library.io) 'mobile/io.platform.mobile.dart'
-if (dart.library.html) 'web/io.platform.web.dart';
+if (dart.library.js) 'web/io.platform.web.dart';
