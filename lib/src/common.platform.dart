1e204fb211be3ef2ddf497440c029afbaf82ff31
gordianknot <gordianknot.data@gmail.com>
2023-02-08 17:29:53 +0800

    Fix entry point of conditional import

--- lib/src/common.platform.dart
+++ lib/src/common.platform.dart
@@ -1,5 +1,5 @@
 library platform;
 
 export 'sketch/platform.loader.dart'
-if (dart.library.io) 'mobile/io.platform.mobile.dart'
-if (dart.library.js) 'web/io.platform.web.dart';
+if (dart.library.io) 'mobile/platform.mobile.dart'
+if (dart.library.js) 'web/platform.web.dart';
